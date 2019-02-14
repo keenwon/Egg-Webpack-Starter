f7a4fe933d51c5b23ccbe7cefb90e04e9826bc1d
keenwon <semanwmj@gmail.com>
2019-02-14 17:38:49 +0800

    build: add speed-measure-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
 const devConfig = require('./client/build/webpack.dev')
 const prodConfig = require('./client/build/webpack.prod')
 
-module.exports = process.env.NODE_ENV === 'development' ? devConfig : prodConfig
+const smp = new SpeedMeasurePlugin()
+
+module.exports = process.env.NODE_ENV === 'development' ? devConfig : smp.wrap(prodConfig)
